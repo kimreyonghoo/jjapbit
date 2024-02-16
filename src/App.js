bc0c2b3ab4eaa4174f01290a77645fafba5591e3
kimreyonghoo <krh6818@naver.com>
2024-02-17 06:53:20 +0900

    24/02/17

--- src/App.js
+++ src/App.js
@@ -30,13 +30,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/jjapbit/' element={
+          <Route  basename={process.env.PUBLIC_URL} path='/' element={
           <div>
             <Topbar></Topbar>
             <Coinlist></Coinlist>
           </div>
           }></Route>
-          <Route path='/jjapbit/:market' element={<CoinChart/>}/>
+          <Route path='/:market' element={<CoinChart/>}/>
         </Routes>
       </BrowserRouter>
     </div>
